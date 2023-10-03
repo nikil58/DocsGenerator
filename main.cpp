4bdc513516b88d532a4c1c2fdf6975a2ec7ed221
nikita <titata1@mail.ru>
2023-10-03 14:18:54 +0300

    fix: high resolution for 4k

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Form.h"
 
 int main(int argc, char *argv[]) {
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
     QApplication a(argc, argv);
     a.setStyleSheet("QSplitter::handle { margin: 15px; background-color: gray }");
     Form mainWindow;
